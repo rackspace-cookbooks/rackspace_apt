6423c648f5b4fc7a3db7bf6c48ccb56041655aac
Kent Shultz <kentmshultz@gmail.com>
2014-01-23 17:06:04 -0600

    v. 3.0.1 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'rackspace-cookbooks@rackspace.com'
 license           'Apache 2.0'
 description       'Configures apt and apt services and LWRPs for managing apt repositories and preferences'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '3.0.0'
+version           '3.0.1'
 recipe            'rackspace_apt', 'Runs apt-get update during compile phase and sets up preseed directories'
 recipe            'rackspace_apt::cacher-ng', 'Set up an apt-cacher-ng caching proxy'
 recipe            'rackspace_apt::cacher-client', 'Client for the rackspace_apt::cacher-ng caching proxy'
